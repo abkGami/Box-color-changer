7a9648abf3b7a44f2d0f9b2742d85630c8fde961
abkgami <yahabubakar1@gmail.com>
2024-10-18 16:17:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var button = document.querySelector('button');
+var box = document.getElementById('changeMe');
+
+function changeColor(){
+    if(box.style.background == 'red'){
+        box.style.background = 'green';
+    }else{
+        box.style.background = 'red'
+    }
+};
